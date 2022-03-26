68673522f3f2ab9b4e3901ae353827b6d6eaedfa
Cọp🐯 <danglegiavu@gmail.com>
2022-03-26 11:07:09 +0700

    add host binding

--- src/app/directive/better-highlight.directive.ts
+++ src/app/directive/better-highlight.directive.ts
@@ -1,27 +1,33 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector:'[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
-    @HostBinding('style.backgroundColor') myColor: string = 'transparent';
 
     constructor(private elementRef: ElementRef, private renderer: Renderer2){
         
     }
+    @HostBinding('style.backgroundColor') myColor: string = 'transparent';
+    @Input() defaultColor: string = 'transparent';
+    @Input("appBetterHighlight") highlightColor : string = 'red';
+
+
     ngOnInit(): void {
-        this.renderer.setStyle(this.elementRef.nativeElement,"background-color","blue")
+        //this.renderer.setStyle(this.elementRef.nativeElement,"background-color","blue")
     }
 
     @HostListener('mouseenter')
     onMouseEnter(){
         //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','red');
-        this.myColor = 'blue'
+        //this.myColor = 'blue'
+        this.myColor = this.defaultColor;
     }
 
     @HostListener('mouseleave')
     onMouseLeave(){
         //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','transparent')
-        this.myColor = 'red'
+        //this.myColor = 'red'
+       this.myColor =  this.highlightColor ;
     }
 }
